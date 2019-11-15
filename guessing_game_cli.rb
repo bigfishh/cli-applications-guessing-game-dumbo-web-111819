eea568afe7e38acfc1c458fcd3a43f531c6480ee
Annie Zheng <anniezheng.anniee@gmail.com>
2019-11-15 21:10:17 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,12 +3,12 @@ def run_guessing_game
   ran_num = rand(6)+1
   ask
   user_input = gets.chomp
-  if user_input == ran_num
+  if user_input == ran_num.to_s
     puts "You guessed the correct number!"
   elsif user_input == "exit"
     puts "Goodbye!"
   else 
-    puts "Sorry! The computer guessed #{ran_num}."
+    puts "Sorry! The computer guessed #{ran_num.to_s}."
   end
     
 end
